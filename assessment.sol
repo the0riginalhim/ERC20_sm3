26ef80d064d8161c0ef9dc32ff4da5c6b28a3ede
the0riginalhim <150500577+the0riginalhim@users.noreply.github.com>
2024-07-10 21:30:56 +0530

    Update assessment.sol

--- assessment.sol
+++ assessment.sol
@@ -1,25 +1,20 @@
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
-contract MyToken is ERC20 {
-  address public owner;
+contract MyToken is ERC20, ERC20Burnable, Ownable, ERC20Permit {
+    constructor(address initialOwner)
+        ERC20("Test", "TST")
+        Ownable(initialOwner)
+        ERC20Permit("MyToken")
+    {}
 
-  constructor(uint256 initialSupply, string memory name, string memory symbol) ERC20(name, symbol) {
-    _mint(msg.sender, initialSupply * 10**decimals());
-    owner = msg.sender;
-  }
-
-  function mint(address to, uint256 amount) public onlyOwner {
-    _mint(to, amount);
-  }
-
-  function burn(uint256 amount) public {
-    _burn(msg.sender, amount);
-  }
-
-  modifier onlyOwner() {
-    require(msg.sender == owner, "Only owner can mint tokens");
-    _;
-  }
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
 }
